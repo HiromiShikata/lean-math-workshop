9d3d09d91c6268983a7d0f1b872e24499f372710
HiromiShikata <shikata.hiromi@gmail.com>
2025-11-15 08:00:48 +0000

    test: add '∀(a,=(a,b))', 'a', '+(1,a)'

--- mathematical_logic_kashimaryo_test.py
+++ mathematical_logic_kashimaryo_test.py
@@ -372,6 +372,7 @@ def test_is_substitution_possible():
     assert is_substitution_possible('∀(y,A)', 'a', '1') == True
     assert is_substitution_possible('∀(y,=(a,1))', 'a', '+(1,a)') == True
     # すでに束縛されている場合はそもそも代入しないから式に変化がないので代入可能
+    assert is_substitution_possible('∀(a,=(a,b))', 'a', '+(1,a)') == True
     assert is_substitution_possible('∀(a,=(a,1))', 'a', '+(1,a)') == True
     assert is_substitution_possible('∀(a,=(b,1))', 'b', '+(1,a)') == False
     assert is_substitution_possible('∃(a,=(b,1))', 'b', '+(0,a)') == False
